93d561c15681a4ad81f7ea675f2c1c9192d68df5
Haider <work.haiderali2002@gmail.com>
2025-06-17 22:23:45 +0530

    Update after removing deployment

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import "./index.css";
 import Layout from "./components/Layout";
 import {
   createBrowserRouter,
-  createRoutesFromChildren,
   createRoutesFromElements,
   Route,
   RouterProvider,
